0968c010867790de4cd80fdbedf6cbf6992d2465
Carlos <contatoengcarlossilveira@gmail.com>
2021-11-22 16:17:15 -0300

    implementando as operações no Projeto Game

--- Projeto_C_Game/game.c
+++ Projeto_C_Game/game.c
@@ -26,17 +26,65 @@ void jogar(){
 }
 
 void mostrarInfo(Calcular calc){
-    //vamos implementar
+    char op[25];
+
+    if(calc.operacao == 0){ // 0 == somar
+        sprintf(op, "Somar");
+    }else if(calc.operacao == 1){//1 == diminuir
+        sprintf(op, "Diminuir"); 
+    }else if(calc.operacao == 2){//2 == multiplicar
+        sprintf(op, "Multiplicar");
+    }else{
+        sprintf(op, "Operação desconhecida");
+    }
+    printf("Valor 1: %d \nValor 2: %d \nDificuldade: %d \nOperação: %s",
+                    calc.valor1, calc.valor2, calc.dificuldade, op);
 }
 
 int somar(int resposta, Calcular calc){
-    //vamos implementar
+    int resultado = calc.valor1 + calc.valor2;
+    calc.resultado = resultado;
+    int certo = 0;//0 == errou, 1 == acertou
+    
+    if(resposta == calc.resultado){
+        printf("Resposta correta!\n");
+        certo = 1;
+    }else{
+        printf("Respota errada!\n");
+    }
+
+    printf("%d + %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+    return certo;
 }
 
 int diminuir(int resposta, Calcular calc){
-    //vamos implementar
+    int resultado = calc.valor1 - calc.valor2;
+    calc.resultado = resultado;
+    int certo = 0;//0 == errou, 1 == acertou
+    
+    if(resposta == calc.resultado){
+        printf("Resposta correta!\n");
+        certo = 1;
+    }else{
+        printf("Respota errada!\n");
+    }
+
+    printf("%d - %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+    return certo;
 }
 
 int multiplicar(int resposta, Calcular calc){
-    //vamos implementar
+    int resultado = calc.valor1 * calc.valor2;
+    calc.resultado = resultado;
+    int certo = 0;//0 == errou, 1 == acertou
+    
+    if(resposta == calc.resultado){
+        printf("Resposta correta!\n");
+        certo = 1;
+    }else{
+        printf("Respota errada!\n");
+    }
+
+    printf("%d * %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+    return certo;
 }
